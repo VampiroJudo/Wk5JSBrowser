6ef2f4a11d18c35549aa7fa06fe5c2f86582db25
John <johnconley507@hotmail.com>
2016-07-12 18:35:10 -0400

    New repo upload

---
+++ app.js
@@ -0,0 +1,16 @@
+// $(document).on('ready', function (){
+// 	//do stuff when the DOM is ready
+// });
+
+
+$(document).ready(function(){
+	$('button').click(function(){
+	$('.muthafucka').fadeToggle(5000);	
+	});
+});
+// var color = true;
+// $(document).ready(function(){
+// 	$('button').click(function(){
+// 	$('unicorn-mode').toggleclass('.rainbow')
+// 	});
+// });
