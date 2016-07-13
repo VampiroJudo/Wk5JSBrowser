46280b298d1ea1d80b87661cf707fec24c30a203
John <johnconley507@hotmail.com>
2016-07-13 12:11:24 -0400

    Making a geoLocator

---
+++ GeoLocation/app.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+	console.log("This should work");
+
+	if ("geolocation" in navigator) {
+		console.log("This browser has geolocation");
+		navigator.geolocation.getCurrentPosition(showPosition, handleError);
+	} 
+	else {
+		console.log("This browser does not have geolocation. :/");
+	}
+
+
+
+function showPosition(position) {
+	console.log("User consented to give location.");
+	console.log(position);
+
+	var lat = position.coords.latitude;
+	var lng = position.coords.longitude;
+
+	var html = `
+	<h2>Your Position</h2>
+	<ul>
+		<li> Latitude: ${lat}</li>
+		<li> Longitude: ${lng}</li>
+	</ul>
+    `;
+    $("body").append(html);
+}
+
+function handleError(error) {
+	console.log("Error getting postion", error);
+
+	if (error.code === 1){
+		var html = `<h2>Come on! What are you scared? It's not like I'm going to hunt you down and... muahahaha...</h2>`;
+		$("body").append(html);
+	}
+
+}
+
+
+});
